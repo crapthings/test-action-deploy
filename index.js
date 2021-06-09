0a0d11f8eb276c9dd9227f28919ae4c78c835ed7
crapthings <crapthings@gmail.com>
2021-06-09 16:43:14 +0800

    update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000
 app.use(express.static('public'))
 
 app.get('/', function (req, res) {
-  res.send('Hello Kitty!')
+  res.send('Hello Kitty! 这是新的更新')
 })
 
 app.listen(port, function () {
